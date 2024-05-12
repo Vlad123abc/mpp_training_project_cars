5d32a04b712b4fb71045500c4f2112356cf57c15
Vlad123abc <vladi.ivanescu@gmail.com>
2024-05-12 17:53:11 +0300

    ServiceProxyTests implemented GetAllCars test

--- network/src/test/java/ServiceProxyTests.java
+++ network/src/test/java/ServiceProxyTests.java
@@ -1,3 +1,4 @@
+import cars.Car;
 import cars.IObserver;
 import cars.User;
 import cars.jsonProtocol.Response;
@@ -9,6 +10,7 @@ import org.mockito.Mockito;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import java.io.*;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -125,5 +127,64 @@ public class ServiceProxyTests {
         assertEquals(user.getUsername(), "vlad");
         assertEquals(user.getPassword(), "parola");
         
+    }
+
+    @Test
+    public void GetAllCarsTest() throws Exception {
+        // setting up input reader - just as we were reading from the socket
+        String responsesSeparatedWithNewLines = "{'type'='OK', 'data'=1}" + System.lineSeparator() + "{'type'='GET_ALL_CARS', 'data'=null}" + System.lineSeparator();
+
+        StringReader responseReader = new StringReader(responsesSeparatedWithNewLines);
+        var bufferedResponseReader = new BufferedReader(responseReader);
+
+        // setting up writer - our ClientWorker will write responses here. We will
+        // assert later what the test output was.
+        StringWriter outputWriter = new StringWriter();
+        var outputPrintWriter = new PrintWriter(outputWriter);
+
+        // Mock objects - these implement interfaces, and they do nothing yet.
+        Closeable mockSocket = Mockito.mock(Closeable.class);
+        IObserver mockObs = Mockito.mock(IObserver.class);
+
+        ServiceProxy serviceProxy = new ServiceProxy(mockSocket, bufferedResponseReader, outputPrintWriter);
+
+        System.out.println("calling login");
+        serviceProxy.login("vlad", "parola", mockObs);
+        System.out.println("called login");
+
+        // check that login succeeded, how do we check this?
+        // well, on error the socket is closed.
+        Mockito.verify(mockSocket, Mockito.never()).close();
+
+
+        System.out.println("calling getAllCars");
+        List<Car> carList = serviceProxy.getAllCars();
+        System.out.println("called getAllCars");
+        assertEquals(0, carList.size());
+
+
+        System.out.println("calling closeConnection");
+        bufferedResponseReader.close();
+        outputPrintWriter.close();
+
+        // check what was written to the server
+        String allResponsesOneResponsePerLine = new String(outputWriter.getBuffer());
+        StringReader whatWasWrittenReader = new StringReader(allResponsesOneResponsePerLine);
+        var whatWasWrittenCheckInput = new BufferedReader(whatWasWrittenReader);
+        var writtenLine = whatWasWrittenCheckInput.readLine();
+        Gson gson = new Gson();
+
+        // was the login sent all?
+        var sentData = gson.fromJson(writtenLine, Response.class);
+        assertEquals(ResponseType.LOGIN, sentData.getType());
+        // ... add verification to see user and pass
+        User user = gson.fromJson(sentData.getData().toString(), User.class);
+        assertEquals(user.getUsername(), "vlad");
+        assertEquals(user.getPassword(), "parola");
+
+        var logoutLine = whatWasWrittenCheckInput.readLine();
+        // was the login sent all?
+        var logoutData = gson.fromJson(logoutLine, Response.class);
+        assertEquals(ResponseType.GET_ALL_CARS, logoutData.getType());
     }
 }
