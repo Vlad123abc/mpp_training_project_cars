02b09fcf36d7ce738de131d18cfbf68571bd0115
Vlad123abc <vladi.ivanescu@gmail.com>
2024-05-12 16:47:41 +0300

    ServiceProxyTests Logout test not working

--- network/src/test/java/ServiceProxyTests.java
+++ network/src/test/java/ServiceProxyTests.java
@@ -62,7 +62,52 @@ public class ServiceProxyTests {
     }
 
     @Test
-    public void LogoutTest() {
+    public void LogoutTest() throws Exception {
+        // setting up input reader - just as we were reading from the socket
+        String responsesSeparatedWithNewLines = "{'type'='OK', 'data'=1}" + System.lineSeparator();
 
+        StringReader responseReader = new StringReader(responsesSeparatedWithNewLines);
+        var bufferedResponseReader = new BufferedReader(responseReader);
+
+        // setting up writer - our ClientWorker will write responses here. We will
+        // assert later what the test output was.
+        StringWriter outputWriter = new StringWriter();
+        var outputPrintWriter = new PrintWriter(outputWriter);
+
+        // Mock objects - these implement interfaces, and they do nothing yet.
+        Closeable mockSocket = Mockito.mock(Closeable.class);
+        IObserver mockObs = Mockito.mock(IObserver.class);
+
+        ServiceProxy serviceProxy = new ServiceProxy(mockSocket, bufferedResponseReader, outputPrintWriter);
+
+        System.out.println("calling logout");
+        User vlad = new User("vlad", "parola");
+        vlad.setId(1L);
+        serviceProxy.logout(vlad, mockObs);
+        System.out.println("called logout");
+
+        // check that login succeeded, how do we check this?
+        // well, on error the socket is closed.
+        Mockito.verify(mockSocket, Mockito.never()).close();
+
+        System.out.println("calling closeConnection");
+        bufferedResponseReader.close();
+        outputPrintWriter.close();
+
+        // check what was written to the server
+        String allResponsesOneResponsePerLine = new String(outputWriter.getBuffer());
+        StringReader whatWasWrittenReader = new StringReader(allResponsesOneResponsePerLine);
+        var whatWasWrittenCheckInput = new BufferedReader(whatWasWrittenReader);
+        var writtenLine = whatWasWrittenCheckInput.readLine();
+        Gson gson = new Gson();
+
+        // was the login sent all?
+        var sentData = gson.fromJson(writtenLine, Response.class);
+        assertEquals(sentData.getType(), ResponseType.LOGOUT);
+
+        // ... add verification to see user and pass
+        User user = gson.fromJson(sentData.getData().toString(), User.class);
+        assertEquals(user.getUsername(), "vlad");
+        assertEquals(user.getPassword(), "parola");
     }
 }
